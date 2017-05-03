219ddc183a0f608414d7eda781ccaaba7762a3f1
Sean Pierce and Sara Schultz <sean@>
2017-05-03 16:45:43 -0700

    begin frint end

--- app.rb
+++ app.rb
@@ -2,11 +2,23 @@ require 'sinatra'
 require 'sinatra/reloader'
 require './lib/book'
 require 'pry'
+require 'pg'
 
 also_reload('lib/**/*.rb')
 
 DB = PG.connect({:dbname => "library_system"})
 
 get('/') do
+  @books = Book.all
+  erb(:index)
+end
+
+post("/add_book") do
+  title = params.fetch('title')
+  author = params.fetch('author')
+  genre = params.fetch('genre')
+  book = Book.new({:title => title, :author => author, :genre => genre})
+  book.save
+  @books = Book.all
   erb(:index)
 end
