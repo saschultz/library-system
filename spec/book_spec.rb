d0eb72ec592f07aa2d250ff4dcf808b24076391a
Sean Pierce and Sara Schultz <sara@>
2017-05-03 13:11:22 -0700

    pass first four specs

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -6,4 +6,24 @@ describe Book do
       expect(Book.all()).to(eq([]))
     end
   end
+
+  describe("#title") do
+    it("tells you its title") do
+      book = Book.new({:title => 'Epicodus Stuff'})
+      expect(book.title).to(eq("Epicodus Stuff"))
+    end
+  end
+  describe("#author") do
+    it("tells you its author") do
+      book = Book.new({:author => 'Sean'})
+      expect(book.author).to(eq("Sean"))
+    end
+  end
+  describe("#genre") do
+    it("tells you its genre") do
+      book = Book.new({:genre => 'Cookbook'})
+      expect(book.genre).to(eq("Cookbook"))
+    end
+  end
+
 end
